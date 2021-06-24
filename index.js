cbef339c2b23a779352ea99e983c407143b71233
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2021-06-25 02:26:11 +0530

    added encrytion func

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var express = require("express");
-var app = express();
-app.listen(8085, () => {
-    console.log("Server running on port 8085");
-   });
-app.post("/test", (req,res) => {
-    var message = req.body;
-    console.log(1);
-    console.log(message);
-    console.log(2);
-    res.json({a:message})
-});
+const params = new URLSearchParams(document.location.search);
+const myNumber = params.get('text');
+$('#input').val(myNumber);
+$('#a').val(myNumber);
+console.log(myNumber);
